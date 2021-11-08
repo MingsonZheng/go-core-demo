4b7c730aad945bd9effd84fcfaf175b9fa9a0f80
郑子铭 <mingsonzheng@outlook.com>
2021-11-08 17:50:59 +0800

    demo101

---
+++ src/048程序性能分析基础/demo96/demo96.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"048程序性能分析基础/common"
+	"048程序性能分析基础/common/op"
+	"errors"
+	"fmt"
+	"os"
+	"runtime/pprof"
+)
+
+var (
+	profileName = "cpuprofile.out"
+)
+
+func main() {
+	f, err := common.CreateFile("", profileName)
+	if err != nil {
+		fmt.Printf("CPU profile creation error: %v\n", err)
+		return
+	}
+	defer f.Close()
+	if err := startCPUProfile(f); err != nil {
+		fmt.Printf("CPU profile start error: %v\n", err)
+		return
+	}
+	if err = common.Execute(op.CPUProfile, 10); err != nil {
+		fmt.Printf("execute error: %v\n", err)
+		return
+	}
+	stopCPUProfile()
+}
+
+func startCPUProfile(f *os.File) error {
+	if f == nil {
+		return errors.New("nil file")
+	}
+	return pprof.StartCPUProfile(f)
+}
+
+func stopCPUProfile() {
+	pprof.StopCPUProfile()
+}
